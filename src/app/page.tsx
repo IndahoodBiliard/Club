8800583334fdef2a80d0d3d7ce3079b5a7b2a7ba
BM Ken <bmken@BMs-iMac-2.local>
2023-09-08 09:58:45 +0700

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
+import MainUi from '@/component/mainUi'
 
 export default function Home() {
-  return <Link href="/saringan">Dashboard</Link>
+  return (
+    <MainUi>
+      <Link href="/saringan">Dashboard</Link>
+    </MainUi>
+  )
 }
