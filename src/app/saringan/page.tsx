5a368da8604773863b5cc42226d1d1fe0d72dd85
BM Ken <bmken@BMs-iMac-2.local>
2023-09-08 08:47:36 +0700

    init

---
+++ src/app/saringan/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <p>
+          Get started by editing&nbsp;
+          <code >src/app/page.tsx</code>
+        </p>
+  )
+}
