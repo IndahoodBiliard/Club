741f1dbbc247c020019abf4be63a4cd64d3346c4
kennguyen997 <dinhvannguyen997@gmail.com>
2023-09-09 22:38:50 +0700

    fix data

--- src/app/beer/[id]/page.tsx
+++ src/app/beer/[id]/page.tsx
@@ -1,11 +1,11 @@
-import { FoodType, foodListData } from '@/data/defaultData'
+import { FoodType, bearListData } from '@/data/defaultData'
 export async function generateStaticParams() {
-  const posts = foodListData
+  const posts = bearListData
   return posts.map((post) => ({
     id: post.id,
   }))
 }
 export default function Page({ params }: { params: { id: string} }) {
-  const dataDetail = foodListData.find(data=> data.id = params.id)
+  const dataDetail = bearListData.find(data=> data.id = params.id)
   return <h1>My Page {params.id} {dataDetail?.name}</h1>
 }
